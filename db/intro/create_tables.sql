c8c3530d95721390ad406ba23a6844d3044f8619
Mike <michaellaishleys@gmail.com>
2024-05-21 19:27:33 +1000

    adjusted code as per intstructions

--- db/intro/create_tables.sql
+++ db/intro/create_tables.sql
@@ -1,15 +1,11 @@
 drop table items;
 drop table categories;
 
--- \c postgres
--- drop database store_db;
--- create database store_db;
--- \c store_db
-
 create table categories (
     id serial primary key,
 
-    name varchar(50) not null,
+    -- unique constraint ensures that two categories can't have the same name
+    name varchar(50) not null unique,
     description text
 );
 
@@ -18,14 +14,30 @@ create table items (
 
     name varchar(100) not null,
     description text not null,
-    categories_id integer not null,
+    -- default value will be used if a new item is created without specifying stock
+    stock int default 0,
+    -- category is now optional ("not null" constraint removed)
+    category_id integer,
 
-    foreign key (categories_id) references categories(id)
+    foreign key (category_id) references categories (id)
 );
 
-insert into categories (name, description) values
+insert into categories (name, description) values 
     ('Electronics', 'Gadgets to make life easier'),
     ('Car Parts', 'Expensive stuff for the box on 4 wheels'),
     ('Sports', 'Get out and play!'),
     ('Video Games', 'Stay in and play!')
-    ;
+;
+
+insert into items (name, description, stock, category_id) values
+    ('Skyrim', 'Awesome open-world RPG', 5, 4),
+    ('World of Warcraft', 'Popular MMORPG', 7, 4),
+    ('iPhone', 'Apple''s flagship smartphone', 12, 1),
+    ('Greg Norman golf clubs', 'At least you can look like a pro', 2, 3)
+;
+
+-- Separate insert statement so we can provide only name and description
+-- In this case, stock will be set to it's default value, and category_id will be null
+insert into items (name, description) values
+    ('Flying DeLorean', 'You built a time machine ... out of a car?!')
+;
