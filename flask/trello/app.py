9ed8042e74557d8ab2c5ec728bdce50d39885e2e
Mike <michaellaishleys@gmail.com>
2024-05-30 13:23:24 +1000

    class updates ORMP2

--- flask/trello/app.py
+++ flask/trello/app.py
@@ -1,21 +1,46 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Text
+from typing import Optional
+from flask_marshmallow import Marshmallow
+
+
+class Base(DeclarativeBase):
+    pass
+
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    "postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello"
+)
 
-db = SQLAlchemy(app)
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+ma = Marshmallow(app)
+
 
 class Card(db.Model):
     __tablename__ = 'cards'
 
-    id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
 
-    title = db.Column(db.String(100))
-    description = db.Column(db.Text())
-    date_created = db.Column(db.Date())
+    # title = db.Column(db.String(100))
+    title: Mapped[str] = mapped_column(String(100))
+    # description = db.Column(db.Text())
+    description: Mapped[Optional[str]] = mapped_column(Text())
+    # date_created = db.Column(db.Date())
+    date_created: Mapped[date]
+
+# Marshmallow schema (NOT a db schema!)
+# Used by Marshmallow to serialize and/or validate our SQLAlchemy models
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'date_created')
 
 
 @app.cli.command('db_create')
@@ -25,9 +50,21 @@ def db_create():
     print('Created tables')
 
     cards = [
-            Card(title='Start the project', description='Stage 1 - Create ERD', date_created=date.today()),
-            Card(title='ORM Queries', description='Stage 2 - Implement CRUD', date_created=date.today()),
-            Card(title='Marshmallow', description='Stage 3 - Implement JSONify of models', date_created=date.today())
+            Card(
+                title='Start the project', 
+                description='Stage 1 - Create ERD', 
+                date_created=date.today()
+            ),
+            Card(
+                title='ORM Queries', 
+                description='Stage 2 - Implement CRUD', 
+                date_created=date.today()
+            ),
+            Card(
+                title='Marshmallow', 
+                description='Stage 3 - Implement JSONify of models', 
+                date_created=date.today(),
+            ),
     ]
 
     db.session.add_all(cards)
@@ -35,18 +72,23 @@ def db_create():
     db.session.commit()
 
 
-@app.cli.command('all_cards')
+@app.route('/cards')
 def all_cards():
-    # select * from cards;
-    stmt = db.select(Card).where(Card.id < 3)
-    cards = db.session.scalars(stmt)
-    for card in cards:    
-        print(card)
+    stmt = db.select(Card)
+    cards = db.session.scalars(stmt).all()
+    return CardSchema(many=True).dump(cards)
 
 
+@app.route('/cards/<int:id>')
+def one_card(id):
+    card = db.get_or_404(Card, id)
+    return CardSchema().dump(card)
 
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.errorhandler(404)
+def not_found(err):
+    return {'ERROR': 'Not Found'}
